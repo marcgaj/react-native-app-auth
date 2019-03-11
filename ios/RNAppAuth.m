183e2ab026b15c63143d14fcc20aaa12da1919af
Marc Gajdosik <marcgaj@gmail.com>
2019-03-11 09:48:29 -0400

    fix typo

--- ios/RNAppAuth.m
+++ ios/RNAppAuth.m
@@ -159,7 +159,7 @@ RCT_REMAP_METHOD(refresh,
   // generates the code_challenge per spec https://tools.ietf.org/html/rfc7636#section-4.2
   // code_challenge = BASE64URL-ENCODE(SHA256(ASCII(code_verifier)))
   // NB. the ASCII conversion on the code_verifier entropy was done at time of generation.
-    NSData *sha265Verifier = [OIDTokenUtilities sha265:codeVerifier];
+    NSData *sha265Verifier = [OIDTokenUtilities sha256:codeVerifier];
   return [OIDTokenUtilities encodeBase64urlNoPadding:sha265Verifier];
 }
 
